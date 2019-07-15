eb09b5918f377b47e9e53cfe5475e5a66ed14551
Viorel Spinu <viorel.spinu@gmail.com>
2019-07-15 17:13:26 +0300

    update

--- google-assistant-cegeka/ga-proxy-server/push-to-talk.sh
+++ google-assistant-cegeka/ga-proxy-server/push-to-talk.sh
@@ -1,8 +1,11 @@
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
 
 ffmpeg -i ./sound_record.wav audio.wav -y
 ffmpeg -i ./audio.wav -ar 11025 -ac 2 output.wav -y
 
-googlesamples-assistant-pushtotalk --project-id nao4-c96b5 --device-model-id nao4-c96b5-nao123-fas3dg --input-audio-file ./output.wav --output-audio-file ./response.wav
+googlesamples-assistant-pushtotalk --project-id $PROJECT_ID --device-model-id $DEVICE_ID --input-audio-file ./output.wav --output-audio-file ./response.wav
+
 ffmpeg -i ./response.wav -ac 2 -filter:a "volume=7" response_robot.wav -y
 
 #afplay ./response_robot.wav
